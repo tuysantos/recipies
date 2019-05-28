879e227521cb0622c97478b8b75a4e98000a40e1
Artur Santos <artur_santos@hotmail.com>
2019-05-28 09:41:45 +0100

    app structure

---
+++ src/app/components/manage-ingredient/manage-ingredient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-ingredient',
+  templateUrl: './manage-ingredient.component.html',
+  styleUrls: ['./manage-ingredient.component.scss']
+})
+export class ManageIngredientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
