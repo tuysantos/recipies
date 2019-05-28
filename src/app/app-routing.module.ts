b2e67d9738df901bde1956d02679f72d8407a57c
Artur Santos <artur_santos@hotmail.com>
2019-05-28 11:20:01 +0100

    added interceptor, cache service and updated appmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { IngredientListComponent } from './components/ingredient-list/ingredient-list.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+
+const routes: Routes = [
+  { path: 'ingredients', component: IngredientListComponent },
+  { path: 'recipes/:id', component: RecipeListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeModule } from './app-recipe.module';
+import { RouterModule } from '@angular/router';
+import { RequestCacheService } from './services/request-cache.service';
+import { CachingInterceptor } from './services/caching-Interceptor';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RecipeModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/ingredients', pathMatch: 'full'},
+      { path: '**', redirectTo: '/ingredients' }
+    ])
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [
+    RequestCacheService,
+    { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
