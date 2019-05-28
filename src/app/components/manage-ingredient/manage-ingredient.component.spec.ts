c4251a03af8b51b2177c1b35d0626298f68a0c49
Artur Santos <artur_santos@hotmail.com>
2019-05-28 11:32:11 +0100

    aded unit tests

--- src/app/components/manage-ingredient/manage-ingredient.component.spec.ts
+++ src/app/components/manage-ingredient/manage-ingredient.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ManageIngredientComponent } from './manage-ingredient.component';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 
 describe('ManageIngredientComponent', () => {
   let component: ManageIngredientComponent;
@@ -8,7 +12,8 @@ describe('ManageIngredientComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageIngredientComponent ]
+      declarations: [ ManageIngredientComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
@@ -21,5 +26,28 @@ describe('ManageIngredientComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the add button disabled', ()=>{
+    component.ingredient = "";
+    const button: DebugElement = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.disabled).toBe(true);
+  });
+
+  it('should have the add button enabled', ()=>{
+    component.ingredient = "garlic";
+    const button: DebugElement = fixture.debugElement.query(By.css('button'));
+    fixture.detectChanges();
+    expect(button.nativeElement.disabled).toBe(false);
+  });
+
+  it('should emit an event', ()=>{
+    component.ingredient = "garlic";
+    spyOn(component.addIngredientEvent, 'emit').and.callThrough();
+    const button: DebugElement = fixture.debugElement.query(By.css('button'));
+    fixture.detectChanges();
+    button.nativeElement.click();
+    expect(component.addIngredientEvent.emit).toHaveBeenCalled();
+    expect(component.addIngredientEvent.emit).toHaveBeenCalledWith('garlic');
   });
 });
--- src/app/components/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaginationComponent } from './pagination.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
@@ -16,10 +18,28 @@ describe('PaginationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+    component.pageSize = 10;
+    component.totalItems = 13;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create 2 pages', ()=> {
+    const liElements: DebugElement[] = fixture.debugElement.queryAll(By.css('li[id=pageElement]'));
+    fixture.detectChanges();
+    expect(liElements.length).toBe(2);
+  });
+
+  it('should have page 2 active', ()=> {
+    spyOn(component.pageChanged, 'emit').and.callThrough();
+    const liElements: DebugElement[] = fixture.debugElement.queryAll(By.css('li[id=pageElement]'));
+    liElements[1].childNodes[0].nativeNode.click();
+    fixture.detectChanges();
+    expect(component.pageChanged.emit).toHaveBeenCalled();
+    expect(component.pageChanged.emit).toHaveBeenCalledWith(2);
+  });
+
 });
